0342ccce641a5ccf26a5cf75f417f0326bd88cb1
James McGrath <jim@sevenpoints.co>
2014-08-31 10:16:13 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleSfdcApp::Application.config.secret_key_base = '652a2746a9b81a51e961e127446fafbd8ebb03bc6fabb0eb3c10d74dcfaa0b01238feab7495edd115bda25c94e8f7b42b5afdd73383bf5bb2954d87109111d0c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SampleSfdcApp::Application.config.session_store :cookie_store, key: '_sample_sfdc_app_session'
